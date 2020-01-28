3351bda5cb1fc275e6985ba66442950c2459844e
dee-sp <60376907+dee-sp@users.noreply.github.com>
2020-01-29 01:17:59 +0500

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -2,90 +2,91 @@
 #include <string.h>
 #include <stdlib.h>
 
-/*struct Book
+struct Book
 {
  char title[50];
  int id;
-};*/
+};
 
 
 struct Node
 {
- //struct Book data;
- int index;
+ struct Book data;
  struct Node* nextnode;
 };
 
 
-void printbook(struct Node* node)
+
+void printbook(struct Book* book)
 { 
- //printf ("%s\n", node->title);
- printf ("%d\n", node->index);
+ printf ("%s\n", book->title);
+ printf ("%d\n", book->id);
 }
 
 
-struct Node* createnode (int input)
+struct Book* createbook (int id)
+{
+ //struct Book book;
+ struct Book* bookptr;
+
+ bookptr = malloc(sizeof(struct Book));
+ bookptr->id = id;
+ strcpy (bookptr->title, "Book");
+ printbook(bookptr);
+
+ return bookptr;
+
+}
+
+struct Node* createnode (int id)
+{
+ //struct Book book;
+ struct Node* nodeptr;
+ struct Book* book;
+
+ //nodeptr = malloc(sizeof(struct Node));
+ book = createbook (id);
+ nodeptr->data = *book;
+
+ return nodeptr;
+
+}
+
+/*void traverselist(struct Node* head) 
+{
+    struct Node* curr = head;
+
+    while (current != NULL) {
+        printf("%d\n", current->val);
+        current = current->next;
+    }
+}*/
+
+struct Node* insertnode (int id2)
 {
  
- struct Node node;
- struct Node* nodeptr = &node;
- 
- //book = malloc(sizeof(struct Book));
- //book->id = 1;
- //strcpy (book->title,"C Plus Plus");
- node.index = input;
- printbook (nodeptr);
- //free (book);
- return &node;
 }
-
-struct Node* insertnode(int input2)
-{
- struct Node node, temp = head;
- struct Node* nodeptr = &node;
-
- for (int i=0; i<4; i++)
- {
-  if(input2 == temp->index)
-  {
-
-  }
- }
-}
-
-
 
 int main ()
 {
- struct Node* head;
- struct Node* curr;
- /*struct Book* book1;
- struct Book* book2;
- struct Book* book3;*/
- //int nodenum = 0;
+    struct Node* head = NULL;
+    struct Node* curr = NULL;
+    struct Book* book;
+    int i=0;
 
- int count = 0;
- head = createnode (count);
- curr = head;
- 
- //for (int i=0; i<5; i++)
-//{
- count++;
- curr->nextnode = createnode (count);
- curr = curr->nextnode;
- 
- count++;
- curr->nextnode = createnode (count);
- curr = curr->nextnode;
-
- count++;
- curr->nextnode = createnode (count);
- curr = curr->nextnode;
- 
- /*for (int i=0; i<5; i++)
-{
- free(book);
-}*/
-
- return 0;
+    for (i=0; i<5; i++)
+    {
+    	if (i=0)
+    	{
+    		curr = head = createnode (i);
+    	}
+    	else
+    	{
+    		curr->nextnode = createnode (i);
+    	    curr = curr->nextnode;
+    	}
+    	
+    }
+    
+	return 0;
 }
