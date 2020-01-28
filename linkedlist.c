3fb92c224271893ff46a8ea77742c0c31ebc4dc3
dee-sp <60376907+dee-sp@users.noreply.github.com>
2020-01-28 22:15:17 +0500

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -2,67 +2,85 @@
 #include <string.h>
 #include <stdlib.h>
 
-struct Book
+/*struct Book
 {
  char title[50];
  int id;
-};
+};*/
 
 
 struct Node
 {
- struct Book data;
- struct Node *nextnode;
+ //struct Book data;
+ int index;
+ struct Node* nextnode;
 };
 
 
-void printbook(struct Book* book)
+void printbook(struct Node* node)
 { 
- printf ("%s\n", book->title);
- printf ("%d\n", book->id);
+ //printf ("%s\n", node->title);
+ printf ("%d\n", node->index);
 }
 
 
-struct Node* createnode (struct Node* node)
+struct Node* createnode (int input)
 {
  
  struct Node node;
- book = malloc(sizeof(struct Book));
- book->id = 1;
- strcpy (book->title,"C Plus Plus");
- printbook (book);
+ struct Node* nodeptr = &node;
+ 
+ //book = malloc(sizeof(struct Book));
+ //book->id = 1;
+ //strcpy (book->title,"C Plus Plus");
+ node.index = input;
+ printbook (nodeptr);
  //free (book);
  return &node;
+}
+
+struct Node* insertnode(int input2)
+{
+ struct Node node, temp = head;
+ struct Node* nodeptr = &node;
+
+ for (int i=0; i<4; i++)
+ {
+  if(input2 == temp->index)
+  {
+
+  }
+ }
 }
 
 
 
 int main ()
 {
- struct Node* head = NULL;
- struct Node* curr = NULL;
- struct Book* book1;
+ struct Node* head;
+ struct Node* curr;
+ /*struct Book* book1;
  struct Book* book2;
- struct Book* book3;
+ struct Book* book3;*/
+ //int nodenum = 0;
 
  int count = 0;
- head = createnode (&book1);
+ head = createnode (count);
  curr = head;
  
  //for (int i=0; i<5; i++)
 //{
  count++;
- curr->nextnode = createnode (&book2);
-curr = 
- printf ("%d\n", count);
-//}
+ curr->nextnode = createnode (count);
+ curr = curr->nextnode;
+ 
  count++;
- curr->nextnode = createnode (&book3);
- printf ("%d\n", count);
+ curr->nextnode = createnode (count);
+ curr = curr->nextnode;
 
  count++;
- curr->nextnode = createnode (&book);
- printf ("%d\n", count);
+ curr->nextnode = createnode (count);
+ curr = curr->nextnode;
  
  /*for (int i=0; i<5; i++)
 {
