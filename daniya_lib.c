f0464dbd250c04791f02226d4bbdde61548b4caa
dee-sp <60376907+dee-sp@users.noreply.github.com>
2020-01-30 15:42:39 +0500

    Add files via upload

---
+++ daniya_lib.c
@@ -0,0 +1,35 @@
+int add (int a, int b)
+{
+	int c;
+	c = a + b;
+	printf ("Addition: %d \n",c);
+
+return c;
+}
+
+int sub (int a, int b)
+{
+	int c;
+	c = a - b;
+	printf ("Subtraction: %d \n",c);
+
+return c;
+}
+
+int mul (int a, int b)
+{
+	int c;
+	c = a * b;
+	printf ("Multiplication: %d \n",c);
+
+return c;
+}
+
+int div (int a, int b)
+{
+	int c;
+	c = a / b;
+	printf ("Division: %d \n",c);
+
+return c;
+}
---
+++ myapp.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main ()
+{
+	int c;
+
+	c=add(2,8);	
+
+return 0;
+}
